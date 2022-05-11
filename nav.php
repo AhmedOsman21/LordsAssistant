e929e17d790720a33864d343015a595d5dcb5a04
AhmedOsman21 <ahmedibrahimxzxzxz@gmail.com>
2022-05-11 19:13:09 +0200

    Refactored home page URL as LordsAssistant/ instead of LordsAssistant/home

--- nav.php
+++ nav.php
@@ -1,13 +1,13 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="home">Lords Assistant</a>
+        <a class="navbar-brand" href="../LordsAssistant/">Lords Assistant</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-sm-0 nav-items">
                 <li class="nav-item main-nav-item">
-                    <a class="nav-link active" aria-current="page" href="home">Home</a>
+                    <a class="nav-link active" aria-current="page" href="../LordsAssistant/">Home</a>
                 </li>
 
                 <li class="nav-item dropdown main-nav-item">
