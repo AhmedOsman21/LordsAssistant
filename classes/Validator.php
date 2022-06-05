165f94cf78c88a394f74146f2fc45bd8b2c59bbc
AhmedOsman21 <ahmedibrahimxzxzxz@gmail.com>
2022-06-05 18:22:29 +0200

    Perform validation logic inside the validate() method to return false if validation failed & true if validation succeed

--- classes/Validator.php
+++ classes/Validator.php
@@ -1,7 +1,7 @@
 <?php
 // Abstract class for input validator class
 abstract class Validation {
-    abstract public function validate(string $value, string  $type) : bool;
+    abstract public function validate(string $value, string  $type): bool;
     abstract protected function clean_input($input);
 }
 
@@ -15,8 +15,32 @@ class Validator extends Validation {
      *
      * @return bool Whether the cleaned-input value is valid or not
      */
-    public function validate(string $value, string $type="") : bool {
+    public function validate(string $value, string $type = ""): bool {
+        // Cleaning the input value
+        $input = $this->clean_input($value);
+        $string_pattern = "/^[A-z\s]*$/";
+        $phone_pattern = "/^(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?$/";
+        $result = false;
 
+
+        // Valid general input
+        if ($type === "") {
+            $result = true;
+
+        // Valid Name
+        } elseif (($type === "name") && preg_match($string_pattern, $input)) {
+            $result = true;
+
+            // Valid E-mail
+        } elseif ($type === "email" && filter_var($input, FILTER_VALIDATE_EMAIL)) {
+            $result = true;
+
+            // Valid Phone Number
+        } elseif ($type === "phone" && preg_match($phone_pattern, $input)) {
+            $result = true;
+        }
+
+        return $result;
     }
 
     /**
