7e211a798737fb0f9f7b1e32410250aa91fecc0c
AhmedOsman21 <ahmedibrahimxzxzxz@gmail.com>
2022-06-05 16:51:56 +0200

    Created Validator class & Created a method to validate inputs based on the  optional parameter

--- classes/Validator.php
+++ classes/Validator.php
@@ -1,6 +1,23 @@
 <?php
 // Abstract class for input validator class
 abstract class Validation {
-    abstract public static function validate(string $value, string  $type) : bool;
+    abstract public function validate(string $value, string  $type) : bool;
     abstract protected function clean_input($input);
+    abstract protected function format_name($name);
+}
+
+// Class to clean input data & validate it
+class Validator extends Validation {
+
+    /** 
+    * Validate input fields based on it's input type to prevent XSS attacks
+    * @param string $value Input data
+    * @param string $type="" (optional) Input type i.e name, email, phone
+    *
+    * @return bool Whether the cleaned-input value is valid or not
+    */
+    public function validate(string $value, string $type="") : bool {
+
+    }
+
 }
