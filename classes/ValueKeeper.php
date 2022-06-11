00cd88269e2ca597f411d528ee62593ec57dfa5b
AhmedOsman21 <ahmedibrahimxzxzxz@gmail.com>
2022-06-11 07:42:51 +0200

    Fixed typo: returned the selected attribute instead of the option value

--- classes/ValueKeeper.php
+++ classes/ValueKeeper.php
@@ -29,7 +29,7 @@ class ValueKeeper {
     */
     public static function keepSelection($selection, $option_value) {
         if ($selection == $option_value) {
-            return $option_value;
+            return "selected";
         }
     }
 }
