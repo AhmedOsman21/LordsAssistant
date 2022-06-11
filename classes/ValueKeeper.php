608776fa60c8e68bad830135c42f2bd4a665c163
AhmedOsman21 <ahmedibrahimxzxzxz@gmail.com>
2022-06-11 07:52:08 +0200

    Check on POST key instead of the variable name

--- classes/ValueKeeper.php
+++ classes/ValueKeeper.php
@@ -28,7 +28,7 @@ class ValueKeeper {
      * @return string 
     */
     public static function keepSelection($selection, $option_value) {
-        if ($selection == $option_value) {
+        if ($_POST[$selection] == $option_value) {
             return "selected";
         }
     }
