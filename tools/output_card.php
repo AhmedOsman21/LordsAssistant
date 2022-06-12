6536750f4959855e0b49860d0dfb2256a055be54
AhmedOsman21 <ahmedibrahimxzxzxz@gmail.com>
2022-06-12 13:24:18 +0200

    Echoing out the function parameters inside card's template

--- tools/output_card.php
+++ tools/output_card.php
@@ -4,11 +4,11 @@ function card($head, $subhead, $bg_color, $txt_color, $output)
 {
   // Card template
   ob_start()?>
-  <div class='card $txt_color $bg_color mb-3' style='max-width: 18rem;'>
-    <div class='card-header'>$head</div>
+  <div class='card <?= $txt_color ?> <?= $bg_color ?> mb-3' style='max-width: 18rem;'>
+    <div class='card-header'> <?= $head ?> </div>
     <div class='card-body'>
-      <h5 class='card-title'>$subhead</h5>
-      <p class='card-text' id='result'>$output</p>
+      <h5 class='card-title'> <?=$subhead ?> </h5>
+      <p class='card-text' id='result'> <?= $output ?> </p>
     </div>
   </div> 
   <?php 
