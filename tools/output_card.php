411a9b1c050f6ea37ff47ab1bc928426896e66dc
AhmedOsman21 <ahmedibrahimxzxzxz@gmail.com>
2022-06-12 13:22:28 +0200

    Separate card's html code and store it in the card variable

--- tools/output_card.php
+++ tools/output_card.php
@@ -2,14 +2,19 @@
 // Make an output card.
 function card($head, $subhead, $bg_color, $txt_color, $output)
 {
-    $card = "
+  // Card template
+  ob_start()?>
   <div class='card $txt_color $bg_color mb-3' style='max-width: 18rem;'>
     <div class='card-header'>$head</div>
     <div class='card-body'>
       <h5 class='card-title'>$subhead</h5>
       <p class='card-text' id='result'>$output</p>
     </div>
-  </div>
-  ";
-    return $card;
+  </div> 
+  <?php 
+  // Card Variable
+  $card = ob_get_clean();
+
+
+  return $card;
 }
