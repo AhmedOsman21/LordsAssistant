8d17b2f4c4d485ce4a8d5617453eb109979efc0d
AhmedOsman21 <ahmedibrahimxzxzxz@gmail.com>
2022-06-17 19:28:18 +0200

    Build calculator's class properties

--- classes/KvkCalculator.php
+++ classes/KvkCalculator.php
@@ -1,5 +1,9 @@
 <?php
 
 class KvkCalculator {
-    
+    private $points;
+    private $rss_type;
+    private $rss_types = array("food" => 120,"stone" => 180,"timber" => 180,"ore" => 240,"gold" => 330);
+    private $rss_amount = 0;
+
 }
