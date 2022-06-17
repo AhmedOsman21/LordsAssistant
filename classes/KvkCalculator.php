2e29ba999321f629c16157079f74cd2d540bcc0c
AhmedOsman21 <ahmedibrahimxzxzxz@gmail.com>
2022-06-17 19:53:24 +0200

    Created Consntructor to take attributes' values and method to calculate the resources amount based on these attributes

--- classes/KvkCalculator.php
+++ classes/KvkCalculator.php
@@ -5,5 +5,20 @@ class KvkCalculator {
     private $rss_type;
     private $rss_types = array("food" => 120,"stone" => 180,"timber" => 180,"ore" => 240,"gold" => 330);
     private $rss_amount = 0;
+    
+    public function __construct($points,  $rss_type, $rss_types) {
+        $this->points = $points;
+        $this->rss_type = $rss_type;
+        $this->rss_types = $rss_types;
+    }
 
+    public function calculate() {
+        // Get Resource Points.
+        $rss_pts = $this->rss_types[$this->rss_type];
+
+        for ($i = 0; $i < $this->points; $i += $rss_pts) {
+            $this->rss_amount += 1000;
+        }
+        return $this->rss_amount;
+    }
 }
